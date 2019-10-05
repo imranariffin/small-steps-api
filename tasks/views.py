f0b04f4dfff96003eeeb80d43d8dec2f11acafa3
Imran Ariffin <ariffin.imran@gmail.com>
2019-10-05 00:28:55 -0400

    Enforce required fields & existing task parent

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,40 @@
+from uuid import UUID
+
 from rest_framework import status
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
+from goals.models import Goal
 from tasks.models import Task
 
 
 @api_view(['POST'])
 @renderer_classes([JSONRenderer])
 def views(request):
-    text = request.data['text']
-    parent_id = request.data['parent_id']
+    try:
+        text = request.data['text']
+        parent_id = request.data['parent_id']
+    except KeyError as ex:
+        return Response(
+            {
+                ex.args[0]: 'This field is required',
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    try:
+        Goal.objects.get(id=UUID(parent_id))
+    except Goal.DoesNotExist:
+        try:
+            Task.objects.get(id=UUID(parent_id))
+        except Task.DoesNotExist:
+            return Response(
+                {
+                    'parent_id': f'Parent with id {parent_id} does not exist',
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
     task = Task.objects.create()
 
