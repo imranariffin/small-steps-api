51dd59a8587dfd648fcb392183d189cc4e69d448
Imran Ariffin <ariffin.imran@gmail.com>
2021-07-11 11:14:07 -0400

    Cleanup (#47)

--- smallsteps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-
-urlpatterns = [
-    path('v1/', include('api.urls', namespace='api')),
-    path('admin/', admin.site.urls),
-]
